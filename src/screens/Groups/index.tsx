ef84c280547ddd23e70912212af1f3c5c9e64bdd
João Trajano <joao.trajanosouza@gmail.com>
2024-08-15 14:13:57 -0300

    fix(version): update react version

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,5 @@
-import { Text, View } from "react-native";
 import { Container } from "./style";
 
 export default function Groups() {
-  return (
-    <Container>
-      <Text>Groups</Text>
-    </Container>
-  );
+  return <Container></Container>;
 }
