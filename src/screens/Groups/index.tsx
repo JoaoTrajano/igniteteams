b6886f574fc0122705ec36c87292af370b84348c
João Trajano <joao.trajanosouza@gmail.com>
2024-08-18 14:56:27 -0300

    feature(list): added flat list for group card

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,13 +2,26 @@ import { Highlight } from "@components/Highlight";
 import { Container } from "./style";
 import { Header } from "@components/Header";
 import { GroupCard } from "@components/GroupCard";
+import { useState } from "react";
+import { FlatList } from "react-native";
+import { ListEmpty } from "@components/ListEmpty";
 
 export default function Groups() {
+  const [groups, setGroups] = useState<string[]>([]);
+
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
-      <GroupCard title="Galera do Ignite" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar uma nova turma?" />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+      />
     </Container>
   );
 }
