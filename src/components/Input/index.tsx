1f01c8cde2ad747ffba87f2887997338845e4587
João Trajano <joao.trajanosouza@gmail.com>
2024-08-19 09:38:44 -0300

    feature(input): style placeholder

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,6 +1,9 @@
+import { useTheme } from "styled-components/native";
 import { Container } from "./style";
 import { TextInputProps } from "react-native";
 
 export function Input({ ...rest }: TextInputProps) {
-  return <Container {...rest} />;
+  const { COLORS } = useTheme();
+
+  return <Container placeholderTextColor={COLORS.GRAY_300} {...rest} />;
 }
