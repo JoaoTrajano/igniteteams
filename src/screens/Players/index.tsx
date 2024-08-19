0e53ddde00e29b275debaf2c32671b58cac01167
João Trajano <joao.trajanosouza@gmail.com>
2024-08-19 15:06:31 -0300

    feature(players): finished players

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
+
 import { Header } from "@components/Header";
-import { Container, Form } from "./style";
+import { Container, Form, HeaderList, NumberOfPlayers } from "./style";
 import { Highlight } from "@components/Highlight";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Input } from "@components/Input";
 import { Filter } from "@components/Filter";
+import { FlatList } from "react-native";
 
 export function Players() {
+  const [team, setTeam] = useState<string>("Time A");
+  const [players, setPlayers] = useState<string[]>(["João Trajano"]);
+
   return (
     <Container>
       <Header showBackButton />
@@ -17,7 +23,21 @@ export function Players() {
         <Input placeholder="Nome da pessoa" autoCorrect={false} />
         <ButtonIcon icon="add" />
       </Form>
-      <Filter title="Time A" />
+      <HeaderList>
+        <FlatList
+          data={["Time A", "Time B"]}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+        <NumberOfPlayers>{players.length}</NumberOfPlayers>
+      </HeaderList>
     </Container>
   );
 }
