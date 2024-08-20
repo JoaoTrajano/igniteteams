8c79ca235ca919fe8f1377281e966b35fde14a92
João Trajano <joao.trajanosouza@gmail.com>
2024-08-20 12:44:09 -0300

    feature(navigation): added navigation in the container

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ import {
 } from "@expo-google-fonts/roboto";
 import { Loading } from "@components/Loading";
 import { StatusBar } from "react-native";
-import { Groups } from "@screens/Groups";
+import { Routes } from "./src/routes";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -19,7 +19,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Groups /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </ThemeProvider>
   );
 }
