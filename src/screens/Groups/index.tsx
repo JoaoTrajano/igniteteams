fc9100a6530782494718cc8bf491cc35cd5011ed
João Trajano <joao.trajanosouza@gmail.com>
2024-08-20 12:50:11 -0300

    feature(navigation): added type navigation

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -6,9 +6,15 @@ import { useState } from "react";
 import { FlatList } from "react-native";
 import { ListEmpty } from "@components/ListEmpty";
 import { Button } from "@components/Button";
+import { useNavigation } from "@react-navigation/native";
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>(["Hell Club"]);
+  const navigation = useNavigation();
+
+  const handleNewGroup = () => {
+    navigation.navigate("groups");
+  };
 
   return (
     <Container>
@@ -23,7 +29,11 @@ export function Groups() {
         )}
         contentContainerStyle={groups.length === 0 && { flex: 1 }}
       />
-      <Button title="Criar nova turma" type="PRIMARY" />
+      <Button
+        title="Criar nova turma"
+        type="PRIMARY"
+        onPress={handleNewGroup}
+      />
     </Container>
   );
 }
